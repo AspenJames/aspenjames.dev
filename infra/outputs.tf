2c50cc782591b46b8c3318c295f82287b39c7a6d
Aspen James <hello@aspenjames.dev>
2023-02-25 20:01:55 -0800

    Infra 301-> aspenjames/ajdev-infra

--- infra/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "dot_dev_ip" {
-  value = linode_instance.dot_dev.ip_address
-}
-
-output "dot_dev_private_key" {
-  value     = tls_private_key.dot_dev.private_key_openssh
-  sensitive = true
-}
--- infra/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "linode_token" {
-  type        = string
-  description = "Linode API token"
-  sensitive   = true
-}
